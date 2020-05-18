b8647b1d76eb96fa8c0dc56eed9f38e3f52eebb6
steveLim0928 <48382745+steveLim0928@users.noreply.github.com>
2020-05-18 22:01:42 +0800

    Add files via upload

---
+++ 18_5_2020_2200/main.cpp
@@ -0,0 +1,101 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QObject>
+#include <QThread>
+
+#include "drawing.h"
+#include "transmit.h"
+#include "receiver.h"
+//#include "serialise.h"
+
+bool sendState = 0; // transmitter state
+bool receiveState = 0; // receiver state
+// 'transmission medium'
+bool charStream[8];
+bool dataStream[88];
+// 3 way handshake
+bool senderReady = 0;
+bool receiverAck = 0;
+bool senderAck = 0;
+
+bool finish = 0;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    drawing userWin;
+    drawing receiveWin;
+
+    userWin.resize(650, 650);
+    receiveWin.resize(650, 650);
+
+    userWin.type(false); // transmitter window
+    receiveWin.type(true); // receiver window
+
+    userWin.setWindowTitle("Draw");
+    receiveWin.setWindowTitle("Receive");
+
+    QThread transmitThread;
+    QThread receiveThread;
+
+    QMutex mutex;
+
+    transmit *transmitter = new transmit();
+    receiver *receive = new receiver();
+
+    // move each object to their threads
+    transmitter->moveToThread(&transmitThread);
+    receive->moveToThread(&receiveThread);
+
+    // Initialise by passing address of each stuff to transmitter and receiver
+    transmitter->init(dataStream, charStream, &sendState, &receiveState,
+                      &senderReady, &receiverAck, &senderAck , &mutex, &finish);
+    receive->init(dataStream, charStream, &sendState, &receiveState,
+                  &senderReady, &receiverAck, &senderAck, &mutex, &finish);
+
+    // Transmit window to transmitter
+    QObject::connect(&userWin, &drawing::sendData, transmitter, &transmit::queue);
+
+    // to enable receiver to receive
+    QObject::connect(&receiveThread, &QThread::started, receive, &receiver::receiveData);
+
+    // Keep receiver running and waiting for data
+    QObject::connect(receive, &receiver::done, receive, &receiver::receiveData);
+
+    QObject::connect(&userWin, &drawing::windowClose, transmitter, &transmit::done);
+
+    // connect from receiver to receive window
+    QObject::connect(receive, &receiver::sendToggle, &receiveWin, &drawing::toggleDraw);
+    QObject::connect(receive, &receiver::sendPosition, &receiveWin, &drawing::onReceivePos);
+    QObject::connect(receive, &receiver::sendPenWidth, &receiveWin, &drawing::onPenSize);
+    QObject::connect(receive, &receiver::sendClear, &receiveWin, &drawing::onClear);
+    QObject::connect(receive, &receiver::sendPenColour, &receiveWin, &drawing::onPenColour);
+    QObject::connect(receive, &receiver::sendEraser, &receiveWin, &drawing::onEraser);
+    QObject::connect(receive, &receiver::sendPen, &receiveWin, &drawing::onPen);
+    QObject::connect(receive, &receiver::sendCanvasSize, &receiveWin, &drawing::onResizeCanvas);
+    QObject::connect(receive, &receiver::sendCanvasColour, &receiveWin, &drawing::onCanvasColour);
+
+    transmitThread.start();
+    receiveThread.start();
+
+    userWin.move(250,150);
+    receiveWin.move(1050,150);
+
+    userWin.show();
+    receiveWin.show();
+
+    a.exec();
+
+    // end Thread and mutex
+
+    transmitThread.quit();
+    transmitThread.terminate();
+
+    receiveThread.quit();
+    receiveThread.terminate();
+    mutex.~QMutex();
+
+    return 0;
+}
---
+++ 18_5_2020_2200/receiver.cpp
@@ -0,0 +1,185 @@
+#include "receiver.h"
+
+receiver::receiver(QObject *parent)
+    : QObject(parent)
+{}
+
+void receiver::init(bool *Data, bool *Char, bool *sendPin, bool *receivePin,
+                    bool *senderReady, bool *receiverAck, bool *senderAck,
+                    QMutex *mtx, bool *finish) { // Initialise all data to have address
+
+    incomeData = Data; // data stream
+    incomeChar = Char; //  char data stream
+    sent = sendPin; // sent state from transmitter
+    receive = receivePin; // receive state from receiver
+    mutex = mtx; // mutex from main
+
+    sReady = senderReady;
+    rAck = receiverAck;
+    sAck = senderAck;
+
+    complete = finish;
+}
+void receiver::receiveData() {
+
+    QDebug debug = qDebug(); // custom qdebug to allow printing without new line
+
+    while(!*sReady) { // waiting for sender to send ready = 1;
+        //debug << "";
+        //QTest::qSleep(1);
+        Sleep(1);
+    }
+
+    mutex->lock();
+    *rAck = 1;
+    qDebug() << "Receiver: ready Ack";
+    mutex->unlock();
+
+    while (!*sAck) { // waiting for sender to send ack = 1;
+        //debug << "";
+        //QTest::qSleep(1);
+        Sleep(1);
+    }
+    qDebug() << "Receiver: Receive sender Ack";
+
+    while(!*sent) { // wait for sent state from transmitter to become 1
+        //debug << "";
+        //QTest::qSleep(1);
+        qDebug() << sent;
+        Sleep(1);
+    }
+    mutex->lock();
+
+    qDebug() << "";
+    qDebug() << "Receiver: Reading Char...";
+    QBitArray readChar;
+    readChar.resize(sizeof (QChar)*8); // to ensure it can hold all bits
+    for (int n=0; n<readChar.size(); n++)
+        {
+
+            bool bit = (incomeChar[n]==1) ? true : false;
+            readChar.setBit(n, bit);
+        }
+
+    // deserialise
+    QByteArray rChar = bittobyte(readChar);
+    QChar c;
+    c = deserial<QChar>(rChar);
+
+    qDebug() << "Receiver: Char received: " << c;
+
+    QBitArray readdata;
+    // to resize readata to ensure it can read all necessary bits
+    if (c == 'p') { // coords
+        readdata.resize(64);
+    }
+    else if (c == 't') { // toggle drawing and last coords
+        readdata.resize(64);
+    }
+    else if (c == 'w') { // pen width
+        readdata.resize(32);
+    }
+    else if (c == 'c') { // pen colour
+        readdata.resize(88);
+    }
+    else if (c == 'e') { // eraser
+        readdata.resize(88);
+    }
+    else if (c == 'P') { // pen
+        readdata.resize(88);
+    }
+    else if (c == 'r') { // resize canvas
+        readdata.resize(64);
+    }
+    else if (c == 'q') { // canvas colour
+        readdata.resize(88);
+    }
+    else if (c == 'C') { // clear canvas
+        readdata.resize(32);
+    }
+
+    qDebug() << "Receiver: Reading Data...";
+    for (int n=0; n<readdata.size(); n++)
+        {
+
+            bool bit = (incomeData[n]==1) ? true : false;
+            readdata.setBit(n, bit);
+        }
+
+    QByteArray rdata = bittobyte(readdata);
+    qDebug() << "Receiver: Data: " << rdata;
+    qDebug() << "";
+
+    if (c == 'p') { // coords
+        emit sendPosition(rdata);
+        qDebug() << "Canvas coordinate updated";
+    }
+    else if (c == 't') { // toggle drawing and last coords
+        emit sendToggle(rdata);
+        qDebug() << "Drawing toggle";
+    }
+    else if (c == 'w') { // pen width
+        emit sendPenWidth(rdata);
+        qDebug() << "Pen width changed";
+    }
+    else if (c == 'c') { // pen colour
+        emit sendPenColour(rdata);
+        qDebug() << "Pen colour changed";
+    }
+    else if (c == 'e') { // eraser
+        emit sendEraser(rdata);
+        qDebug() << "Eraser selected";
+    }
+    else if (c == 'P') { // eraser
+        emit sendPen(rdata);
+        qDebug() << "Pen selected";
+    }
+    else if (c == 'r') { // reisze canvas
+        emit sendCanvasSize(rdata);
+        qDebug() << "Canvas resized";
+    }
+    else if (c == 'q') { // reisze canvas
+        emit sendCanvasColour(rdata);
+        qDebug() << "Canvas colour changed";
+    }
+    else if (c == 'C') { // clear canvas
+        emit sendClear(rdata);
+        qDebug() << "Clear canvas";
+    }
+    qDebug() << "";
+
+    *receive = 1; // notify transmitter that receiver has received datastream
+    qDebug() << "Receiver: Receive set";
+    *rAck = 0;
+    mutex->unlock();
+
+    while (*sent) { // wait for sent state from transmitter to become 0, indicate reset
+        //debug << "";
+        //QTest::qSleep(1);
+        Sleep(1);
+    }
+    mutex->lock();
+    *receive = 0; // reset back receive state
+    qDebug() << "Receiver: Receive reset";
+    mutex->unlock();
+
+    if (!*complete) {
+        emit done(); // call same function again, prepare for another receive when transmitter is ready
+    }
+    else {
+        qDebug() << "Cleaning done";
+        emit threadQuit();
+    }
+}
+QByteArray receiver::bittobyte(QBitArray convert)
+{
+    QByteArray bytes;
+    bytes.resize(convert.count()/8+1);
+    bytes.fill(0);
+
+    for(int b=0; b<convert.count(); ++b)
+    {
+        bytes[b/8] = ( bytes.at(b/8) | ((convert[b]?1:0)<<(7-(b%8))));
+    }
+    return bytes;
+}
